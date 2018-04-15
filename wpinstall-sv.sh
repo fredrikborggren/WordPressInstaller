1c51556fa7afda59175a8210267b521111b19fc6
Fredrik Borggren <fredrik.borggren@me.com>
2018-04-15 13:01:51 +0200

    adding swedish version

---
+++ wpinstall-sv.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+clear
+echo "=================================================="
+echo "Installing latest version of WordPress (Swedish)"
+echo "=================================================="
+
+# Download latest wordpress archive
+curl -O https://sv.wordpress.org/latest-sv_SE.tar.gz
+
+# Extract latest WordPress archive
+tar -zxvf latest.tar.gz
+
+# Change current directory to extracted WordPress archive
+cd wordpress
+
+# Remove default htaccess from extracted WordPress archive
+if [ -f .htaccess ]; then
+    rm .htaccess
+fi
+
+# Remove default config from extracted WordPress archive
+if [ -f wp-config-sample.php ]; then
+    rm wp-config-sample.php
+fi
+
+# Remove wp-content folder from extracted WordPress archive
+rm -R wp-content
+
+# Copy files from extracted WordPress archive to installation directory
+cp -rf . ..
+
+# Go back to installation directory
+cd ..
+
+# Remove folder created by extracting WordPress archive
+rm -R wordpress
+
+# Remove latest WordPress archive
+rm latest.tar.gz
+
+echo "=================================================="
+echo "Installation completed"
+echo "=================================================="
